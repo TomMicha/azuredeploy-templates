7adc43722938ed9e860984369d6664ab04e3edf6
Tom Michalik <tommichalk@sagence-l-jharr.lan>
2020-03-18 10:46:25 -0500

    first step + nsg

---
+++ NSG/deployNSG.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+location="eastus"
+resourceGroup=ALAS-nsg
+DeployName=ALAS-nsg
+networkgroup=ALAS-nsg
+template="azuredeploy.json"
+templateParams="azuredeploy.parameters.json"
+
+echo $location
+az group create -n $resourceGroup -l $location \
+    --verbose
+
+
+# create template deployment
+az group deployment create \
+    --name $DeployName \
+    --resource-group $resourceGroup \
+    --template-file $template \
+    --parameters $templateParams \ 
+    --verbose
