05f982c98c13d512cc47bbbeb73e11e279bfa65c
Tom Michalik <tommichalk@sagence-l-jharr.lan>
2020-03-19 21:31:45 -0500

    test sql net deployment

--- sql_net/netstore-script.sh
+++ sql_net/netstore-script.sh
@@ -1,8 +1,38 @@
 #!/bin/bash
 # create key vault in designated environment
-
 location="eastus"
+keyResourceGroup=ALAS-keyvault
+keyDeploymentName=ALAS-keyvault
+keyVault=ALAS-keyvault
+password=ALASdiadb123
+secretname=sqlServerAdminPass
+echo $password
+echo $location
+az group create -n $keyResourceGroup -l $location \
+    --verbose
+
+#create key vault
+az keyvault create \
+    --name $keyVault \
+    --resource-group $keyResourceGroup \
+    --location $location \
+    --enabled-for-template-deployment true
+
+# add password to keyvault
+securepassword=$(printf $password | base64)
+echo $securepassword
+
+# set the secret in keyvault
+az keyvault secret set \
+    --name $secretname \
+    --vault-name $keyVault \
+    --value $securepassword \
+    --verbose
+
+#create resource group and template links to resources
+#location="eastus"
 resourceGroup=test-vnet-storage-mix
+databaseResourceGroup=ALAS-databases-dev
 DeployName=test-vnet-storage-mix
 networkgroup=test-vnet-storage-mix
 template="https://raw.githubusercontent.com/TomMicha/azuredeploy-templates/master/sql_net/azuredeploy.json"
@@ -11,7 +41,8 @@ templateParams="https://raw.githubusercontent.com/TomMicha/azuredeploy-templates
 echo $location
 az group create -n $resourceGroup -l $location \
     --verbose
-
+az group create -n $databaseResourceGroup -l $location \
+    --verbose
 
 # create template deployment
 az group deployment create \
--- sql_net/readme.md
+++ sql_net/readme.md
@@ -4,7 +4,7 @@ current resources:
 NSG
 VNET ( 4 subnets, main one associated with NSG )
 storage account
-+sql server 
++sql server (save password to keyvault for retrieval during deployment)
 +sql database
 +storage (datalake)
 
@@ -14,3 +14,13 @@ Add storage account (data lake) + Sql database + server
 issues:
 cant seem to add additional security rules for PL and Allyson IP's
 says Ruls cannot have same Priority and Direction even though they do not..
+
+
+Reference Key Vault secrets how:
+"adminPassword": {
+        "reference": {
+          "keyVault": {
+          "id": "/subscriptions/<subscription-id>/resourceGroups/<rg-name>/providers/Microsoft.KeyVault/vaults/<vault-name>"
+          },
+          "secretName": "ExamplePassword"
+        }
