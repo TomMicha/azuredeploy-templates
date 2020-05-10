4388be6609cb1432f64a2a2606e13630ca42c761
Tom Michalik <tommichalk@sagence-l-jharr.lan>
2020-05-09 19:32:16 -0500

    add vm templated add directory

---
+++ vm-within-vnet/readme.md
@@ -0,0 +1,21 @@
+# ALAS-5-05-20-dataplatform-template 
+
+!!this should deploy VMs with network secuirty group, inside vnet, storage account (regular)
+
+current resources:
+NSG (attached to subnet1 and vnet)
+VNET ( already created )
+storage account
+Public IPs
+Network Interface
+
+
+# Instructions
+modify variables in parameters file and scripts to point to template artifact URI
+run bash script
+
+# REMOVE NETWORK SECURITY RULES ACLS
+(also removed NSG from databricks subnets)
+---datalake----
+
+      
---
+++ vm-within-vnet/vmGroupScript.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#create key vault secret for deployment
+location="eastus"
+vmResourceGroup=ALAS-External-Workspace-dev
+vmDeployName=ALAS-keyvault-deployWorkspace-dev
+keyVault=ALASDIA-keyvault-dev
+template="https://raw.githubusercontent.com/TomMicha/azuredeploy-templates/master/vm-within-vnet/azuredeploy.json"
+templateParams="https://raw.githubusercontent.com/TomMicha/azuredeploy-templates/master/vm-within-vnet/azuredeploy.parameters.json"
+
+
+az group create -n $vmResourceGroup -l $location \
+    --verbose
+
+# create template deployment
+az group deployment create \
+    --name $vmDeployName \
+    --resource-group $vmResourceGroup \
+    --template-uri $template \
+    --parameters $templateParams \
+    --debug
+
+
+
+# set the secret in keyvault
+#az keyvault secret set \
+#    --name $secretname \
+#    --vault-name $keyVault \
+#    --value $securepassword \
+#   --verbose
