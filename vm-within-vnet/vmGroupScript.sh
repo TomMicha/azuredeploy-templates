3395a636d3b25c1d1ff6b1f5c60d6e73d1b64ff8
Tom Michalik <tommichalk@sagence-l-jharr.lan>
2020-05-11 14:54:13 -0500

    fix keyvualt script

--- vm-within-vnet/vmGroupScript.sh
+++ vm-within-vnet/vmGroupScript.sh
@@ -7,7 +7,7 @@ vmDeployName=ALAS-deployWorkspace-dev
 template="https://raw.githubusercontent.com/TomMicha/azuredeploy-templates/master/vm-within-vnet/azuredeploy.json"
 templateParams="https://raw.githubusercontent.com/TomMicha/azuredeploy-templates/master/vm-within-vnet/azuredeploy.parameters.json"
 
-az keyvault secret set --vault-name ALAS-keyvaultgroup-dev --secret-name externalVM --value HBRgroup2020 --verbose
+az keyvault secret set --name externalVMPass --vault-name ALAS-keyvaultgroup-dev --value HBRgroup2020 --verbose
 
 az group create -n $vmResourceGroup -l $location \
     --verbose
