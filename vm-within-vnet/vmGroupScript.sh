6b7e20aa40427c53429cdd404ab848cdda25226a
Tom Michalik <tommichalk@sagence-l-jharr.lan>
2020-05-11 15:01:57 -0500

    fix kv isue

--- vm-within-vnet/vmGroupScript.sh
+++ vm-within-vnet/vmGroupScript.sh
@@ -7,7 +7,7 @@ vmDeployName=ALAS-deployWorkspace-dev
 template="https://raw.githubusercontent.com/TomMicha/azuredeploy-templates/master/vm-within-vnet/azuredeploy.json"
 templateParams="https://raw.githubusercontent.com/TomMicha/azuredeploy-templates/master/vm-within-vnet/azuredeploy.parameters.json"
 
-az keyvault secret set --name externalVMPass --vault-name ALAS-keyvaultgroup-dev --value HBRgroup2020 --verbose
+az keyvault secret set --name externalVMPass --vault-name ALAS-keyvault-dev --value HBRgroup2020 --verbose
 
 az group create -n $vmResourceGroup -l $location \
     --verbose
