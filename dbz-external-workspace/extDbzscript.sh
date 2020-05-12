9937ed54b300686dd4d65d8c57d0d05f71f12ad8
Tom Michalik <tommichalk@sagence-l-jharr.lan>
2020-05-12 18:52:14 -0500

    add external DS env

---
+++ dbz-external-workspace/extDbzscript.sh
@@ -0,0 +1,19 @@
+# script shell for databricks external space - redefine subnets add bastion
+
+location="eastus"
+dbzResourceGroup=ALAS-DS-Workspace-dev
+dbzDeployName=ALAS-deploy-DS-dev
+
+template="https://raw.githubusercontent.com/TomMicha/azuredeploy-templates/master/dbz-external-workspace/azuredeploy.json"
+templateParams="https://raw.githubusercontent.com/TomMicha/azuredeploy-templates/master/dbz-external-workspace/azuredeploy.parameters.json"
+
+az group create -n $dbzResourceGroup -l $location \
+    --verbose
+
+# deploy dbz workspace (without cluster)
+az group deployment create \
+    --name $dbsDeploymentName \
+    --resource-group $dbzResourceGroup \
+    --template-uri $template \
+    --parameters $templateParams \
+    --debug
